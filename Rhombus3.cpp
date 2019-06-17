4504516c3fc426b3d19eb4f9eda9e50e97191991
SIDxOFFICIAL <40460719+SIDxOFFICIAL@users.noreply.github.com>
2019-06-17 14:45:55 +0530

    Add files via upload

---
+++ Rhombus3.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+    int i=1;
+    int stars=1, spaces=n-1;
+    while (i<=2*n-1){
+        ///multiple spaces
+        int j=1;
+        while (j<=spaces){
+            cout<<" ";
+            j=j+1;
+        }
+        ///multiple stars
+        int k=1;
+        while (k<=stars){
+            cout<<"*";
+            k=k+1;
+        }
+        ///next line
+        cout<<endl;
+        if (i<n) {
+            stars=stars+2;
+            spaces=spaces-1;
+        } else {
+            stars=stars-2;
+            spaces=spaces+1;
+        }
+        i=i+1;
+    }
+}
